75056a9dc07127d7e4811efc0c404ec817e99d69
FrancoMCuello <franco.m.cuello@gmail.com>
2024-07-10 14:40:33 -0300

    router

--- front/portfolioFront/src/App.tsx
+++ front/portfolioFront/src/App.tsx
@@ -1,12 +1,23 @@
 import Home from "./components/home";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <body>
-      <Home />
-    </body>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
+
+//Herramienta LINK para navegar sin recargar la pagina, es parecido a la etiqueta HTML <a></a>
+
+//<Link className="prueba" to="/rutaQueDeseo> Descripcion</Link>;
---
+++ front/portfolioFront/src/components/contact/index.tsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <>
+      <h1>Hola soy el contact</h1>
+    </>
+  );
+}
+
+export default Contact;
